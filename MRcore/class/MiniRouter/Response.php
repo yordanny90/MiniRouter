5ea2faac61485ff064e2790d9beecef36c53709a
yordanny90 <yordanny90@gmail.com>
2022-09-02 21:05:49 -0600

    Mejoras de librerías
    - Mejora de Response.php

--- MRcore/class/MiniRouter/Response.php
+++ MRcore/class/MiniRouter/Response.php
@@ -1,18 +1,18 @@
 <?php
+
 namespace MiniRouter;
 
 class Response{
 	protected static $default_content_type='text/plain';
 
 	protected $http_code=200;
-	protected $content_type;
-	protected $content='';
+	protected $content;
 	protected $extraHeaders=[];
 	protected $includeBuffer=false;
-	protected $nocache=true;
+	protected $keepAlive;
 
 	public function __construct($content_type=null){
-		$this->set_content_type(is_string($content_type)?$content_type:static::$default_content_type);
+		$this->content_type($content_type);
 	}
 
 	/**
@@ -68,7 +68,7 @@ class Response{
 	 * @see ob_end_clean()
 	 */
 	public static function clearBuffer(){
-		while(ob_get_level()) ob_end_clean();
+		while(ob_get_level()>0) ob_end_clean();
 	}
 
 	/**
@@ -76,8 +76,8 @@ class Response{
 	 * @see ob_end_flush()
 	 * @see flush()
 	 */
-	public static function sendBuffer(){
-		while(ob_get_level()) ob_end_flush();
+	public static function flushBuffer(){
+		while(ob_get_level()>0) ob_end_flush();
 		flush();
 	}
 
@@ -103,24 +103,60 @@ class Response{
 		return $this->includeBuffer;
 	}
 
-	public function send($includeBuffer=false){
-		if(static::headers_sent()) return false;
-		if(!$includeBuffer) static::clearBuffer();
-		ob_start();
-		echo $this->content;
-		static::flatBuffer();
-		static::addHeaders($this->extraHeaders);
-		header('Content-Type: '.$this->content_type);
-		header('Content-Length: '.ob_get_length());
-		header('Connection: close', true, $this->http_code);
-		http_response_code($this->http_code);
-		static::sendBuffer();
-		return true;
+	/**
+	 * No se recomienda usar con HTTP/2
+	 * @param $val
+	 * @return bool
+	 */
+	public function keepAlive($val=null){
+		if(!is_null($val)) $this->keepAlive=boolval($val);
+		return $this->includeBuffer;
 	}
 
-	public function send_exit($includeBuffer=false){
-		$this->send($includeBuffer);
-		exit;
+	public function content_size(){
+		if(is_string($this->content)){
+			return strlen($this->content);
+		}
+		elseif(is_object($this->content) && is_string($this->content->file ?? null) && is_int($this->content->size ?? null)){
+			return $this->content->size;
+		}
+		return 0;
+	}
+
+	private function flushContent(){
+		if(is_string($this->content)){
+			echo $this->content;
+			flush();
+		}
+		elseif(is_array($this->content) && is_string($this->content->file ?? null) && is_int($this->content->size ?? null)){
+			if(!($this->content->res ?? null)){
+				$this->content->res=fopen($this->content->file ?? null, 'r', false, $this->content->context ?? null);
+			}
+			if($this->content->res){
+				fpassthru($this->content->res);
+				flush();
+				fclose($this->content->res);
+			}
+		}
+	}
+
+	public function send(){
+		if(static::headers_sent()) return false;
+		$length=$this->content_size();
+		if($this->includeBuffer){
+			static::flatBuffer();
+			$length+=ob_get_length();
+		}
+		else{
+			static::clearBuffer();
+		}
+		static::addHeaders($this->extraHeaders);
+		if($this->keepAlive===false) header('Content-Length: '.$length);
+		if(is_bool($this->keepAlive)) header('Connection: '.($this->keepAlive?'keep-alive':'close'), true, $this->http_code);
+		http_response_code($this->http_code);
+		static::flushBuffer();
+		$this->flushContent();
+		return true;
 	}
 
 	function &http_code($http_code){
@@ -130,8 +166,44 @@ class Response{
 		return $this;
 	}
 
-	function &content($content){
-		if(is_string($content)) $this->content=$content;
+	function &download(string $name='download.tmp'){
+		$this->extraHeaders['Content-Disposition']='attachment; filename='.$name;
+		return $this;
+	}
+
+	function &noDownload(){
+		unset($this->extraHeaders['Content-Disposition']);
+		return $this;
+	}
+
+	function hasContent(){
+		return !is_null($this->content);
+	}
+
+	function &noContent(){
+		$this->content=null;
+		$this->http_code(204);
+		return $this;
+	}
+
+	function &content(string $content){
+		$this->content=$content;
+		return $this;
+	}
+
+	function &contentFile(string $filename, $context=null){
+		$res=fopen($filename, 'r', false, $context);
+		if($res){
+			$this->content=(object)[
+				'res'=>$res,
+				'size'=>fstat($res)['size'],
+				'file'=>$filename,
+				'context'=>$context,
+			];
+			$this->http_code(200);
+		}
+		$this->noContent();
+		$this->http_code(204);
 		return $this;
 	}
 
@@ -149,9 +221,18 @@ class Response{
 		return $this;
 	}
 
+	function &noCache(){
+		return $this->cache(0);
+	}
+
+	/**
+	 * @param int $max_age Para desactivar la cache se establece en cero (0)
+	 * @return $this
+	 */
 	function &cache(int $max_age){
 		if($max_age>0){
 			$this->extraHeaders['Cache-Control']='max-age='.$max_age;
+			unset($this->extraHeaders['Pragma']);
 		}
 		else{
 			$this->extraHeaders['Cache-Control']='no-store, no-cache, must-revalidate, max-age=0';
@@ -168,26 +249,37 @@ class Response{
 	}
 
 	public function get_content_type(){
-		return $this->content_type;
+		return $this->extraHeaders['Content-Type'] ?? null;
 	}
 
-	public function set_content_type($content_type){
-		$this->content_type=strval($content_type);
+	public function &content_type(?string $content_type){
+		if(is_string($content_type)) $this->extraHeaders['Content-Type']=$content_type;
+		else unset($this->extraHeaders['Content-Type']);
+		return $this;
+	}
+
+	static function &file($filename, $context=null, $mime=null): self{
+		return (new static($mime ?? 'application/octet-stream'))->download(basename($filename))->contentFile($filename, $context);
 	}
 
 	static function &json($data): self{
 		return (new static('application/json'))->content(json_encode($data));
 	}
 
-	static function &text($text): self{
+	static function &text(string $text): self{
 		return (new static('text/plain'))->content($text);
 	}
 
-	static function &html($html): self{
+	static function &html(string $html): self{
 		return (new static('text/html'))->content($html);
 	}
 
-	static function &redirect($location): self{
-		return (new static())->headers(['location'=>$location])->http_code(302);
+	static function &redirect(string $location): self{
+		return (new static())->headers(['location'=>$location])->http_code(302)->cache(0);
 	}
+
+	static function &empty(): self{
+		return (new static())->noContent();
+	}
+
 }
