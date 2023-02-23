bda9a40a1cd088d5e1afae192a6b86d62bba8ace
yordanny90 <yordanny90@gmail.com>
2023-02-23 10:07:01 -0600

    Mejoras
    - Ahora el archivo .MRcore.phar.gz no contiene el php que lo crea

--- makePHAR.php
+++ makePHAR.php
@@ -1,13 +1,13 @@
 <?php
-$phar_file=__DIR__.'/../.MRcore.phar';
+$phar_file=__DIR__.'/.MRcore.phar';
 //readfile(__DIR__.'/phar_loader.php');
 $phar=new Phar($phar_file, 0, 'MRcore.phar');
-$phar->buildFromDirectory(__DIR__);
+$phar->buildFromDirectory(__DIR__.'/.MRcore');
 unlink($phar_file.'.gz');
 $phar->compress(Phar::GZ);
 $phar=null;
 unlink($phar_file);
-$phar=new Phar(__DIR__.'/../.MRcore.phar.gz', 0, 'MRcore.phar');
+$phar=new Phar(__DIR__.'/.MRcore.phar.gz', 0, 'MRcore.phar');
 $phar->setStub(<<<PHP
 <?php
 Phar::mapPhar("MRcore.phar");
