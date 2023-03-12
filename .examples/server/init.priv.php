d918b1e10ca3f830baffc6eee35d1ac728f18704
yordanny90 <yordanny90@gmail.com>
2023-03-12 07:49:59 -0600

    Delete .examples/server directory

--- .examples/server/init.priv.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('BASE_URL', 'http://localhost/'); // URL hasta la carpeta raíz del proyecto
-define('DEFAULT_SCRIPT', 'index.php'); // Script por defecto del servidor
-define('TITLE', 'Example');
