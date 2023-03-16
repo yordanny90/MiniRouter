3f588d6c01d2d92d152e099944f61de37ce7a8ad
yordanny90 <yordanny90@gmail.com>
2023-03-15 23:25:41 -0600

    Reordenamiento de MRcore
    - Corrección en el estandar de argumentos cli
    - Mejora del makePHAR.php
    - MEjora para la carga de los samples cli y http

--- .MRcore/loader.php
+++
@@ -1,4 +0,0 @@
-<?php
-if(!define('MRCORE_DIR', __DIR__)) throw new Exception('MRcore already loaded', 1);
-require_once MRCORE_DIR.'/helper/classloader_helper.php';
-\MiniRouter\classloader(MRCORE_DIR.'/class', '', '.php', 'MiniRouter', true);
---
+++ src/sample/router_cli.php
@@ -0,0 +1,28 @@
+<?php
+
+use MiniRouter\Request;
+use MiniRouter\Response;
+use MiniRouter\RouteException;
+use MiniRouter\Router;
+use function MiniRouter\classloader;
+
+if(!defined('APP_DIR')) throw new Exception('App dir missing', 1);
+Response::flatBuffer();
+if(!Request::isCLI()){
+	Response::r_forbidden()->send();
+	exit;
+}
+try{
+	$router=Router::startCli('AppTask');
+	classloader(APP_DIR.'/Routes', '', '.php', $router->getMainNamespace(), true);
+	$router->prepare();
+	global $ROUTE;
+	$ROUTE=$router->getRoute();
+	unset($router);
+	// Se encontró la función que se ejecutará
+	// Aqui pueden realizar validaciones de seguridad antes de ejecutar el endpoint
+	// Conexiones a bases de datos, u otros servicios externos que puedan retrazar la ejecución
+	$ROUTE->call();
+}catch(RouteException $e){
+	$e->getResponse()->send();
+}
