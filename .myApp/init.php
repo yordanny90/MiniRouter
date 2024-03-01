5b7abfa9a59ee0a2be6433312843a7cc9658a41f
yordanny90 <yordanny90@gmail.com>
2024-02-29 22:13:25 -0600

    MRcore
    - Corrección de referencias

--- .myApp/init.php
+++ .myApp/init.php
@@ -4,8 +4,9 @@ if(!define('BASE_DIR', realpath(__DIR__.'/..'))) throw new Exception('Base alrea
 if(!chdir(BASE_DIR)) throw new Exception('Current dir not changed', 1);
 
 // Puede usar la version comprimida para ahorrar espacio:
-//require BASE_DIR.'/.MRcore.phar.gz';
-require BASE_DIR.'/.MRcore.phar';
+//require BASE_DIR.'/build/MRcore.phar.gz';
+require BASE_DIR.'/build/MRcore.phar';
+//require BASE_DIR.'/src/loader.php';
 
 \MiniRouter\classloader(APP_DIR.'/class');
 //\MiniRouter\classloader(BASE_DIR.'/.lib_class');
